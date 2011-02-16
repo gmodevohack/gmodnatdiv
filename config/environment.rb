7b6723163e149da0a741bfca3414a939be284190
seth redmond <seth@SethBook.local>
2011-02-16 18:04:23 +0000

    added workaround for use with rails >= 1.5.0
    see (see: https://rails.lighthouseapp.com/projects/8994/tickets/4026-rubygems-136-wa
    rning-in-rails-235) for details

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,22 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+
+# workaround for gem versions >= 1.5.0
+if Gem::VERSION >= "1.3.6" 
+    module Rails
+        class GemDependency
+            def requirement
+                r = super
+                (r == Gem::Requirement.default) ? nil : r
+            end
+        end
+    end
+end
+
+
+
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -20,7 +36,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-   config.gem "ruby-debug"
+   config.gem "ruby-debug"    	 # comment out if >= ruby 1.8.x
+#   config.gem "ruby-debug19"	 # comment out if >= ruby 1.9.x
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
